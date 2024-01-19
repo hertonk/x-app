7cc1d1450aeb078f86cdd4da0177ecd15d7c115f
Herton Vilarim <hertonk@gmail.com>
2024-01-19 18:53:57 -0300

    ForgotPassword fix

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -12,7 +12,7 @@ export function Login(){
                     <button className='bg-vinho px-4 py-2 rounded-md text-white'>ENTRAR</button>
                 </form>
                 <div className='my-10'>
-                    <a href="#" className='text-vinho'>Esqueceu sua senha?</a>
+                    <a href="/esqueceu-sua-senha" className='text-vinho'>Esqueceu sua senha?</a>
                 </div>
             </div>
         </div>
