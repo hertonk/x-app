f347f5f1ad3589a484283ee82b8f23d18facc12e
Herton Vilarim <hertonk@gmail.com>
2024-01-19 18:56:09 -0300

    ForgotPassword fix1

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -7,7 +7,7 @@ export function ForgotPassword(){
                 <img src={logo} alt="Logotipo X do Infinito" width={200} className='block' />
                 <h1 className='text-vinho font-bold mt-10 mb-3'>Esqueceu sua senha?</h1>
                 <form className='flex flex-col'>
-                    <p className='text-center mt-3'>Digite seu e-mail abaixo</p>
+                    <p className='text-center mt-3'>Digite seu e-mail</p>
                     <input type="text" name="" id="" placeholder='E-mail' className='my-2 rounded-md px-4 py-2 border-2 border-marrom' />
                     <button className='bg-vinho px-4 py-2 rounded-md text-white'>RECUPERAR</button>
                 </form>
