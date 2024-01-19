8695b2a6a93daa0b8f4a7d1a3ffcf679dfdf1652
Herton Vilarim <hertonk@gmail.com>
2024-01-19 18:51:01 -0300

    ForgotPassword

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 
 import { Login } from './pages/Login';
+import { ForgotPassword } from './pages/ForgotPassword';
 
 export default function Router(){
     return (
         <Routes>
             <Route path='/' element={<Login />}/>
+            <Route path='/esqueceu-sua-senha' element={<ForgotPassword />}/>
         </Routes>
     );
 }
--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,19 +1,15 @@
 import logo from '../assets/logo.png';
 
-export function Login(){ 
+export function ForgotPassword(){ 
     return  (
         <div className="bg-bege w-screen h-screen flex items-center justify-center">
             <div className="w-96 mx-auto flex flex-col justify-center items-center">
                 <img src={logo} alt="Logotipo X do Infinito" width={200} className='block' />
-                <h1 className='text-vinho font-bold mt-10 mb-3'>Acesse sua conta</h1>
+                <h1 className='text-vinho font-bold mt-10 mb-3'>Esqueceu sua senha?</h1>
                 <form className='flex flex-col'>
                     <input type="text" name="" id="" placeholder='E-mail' className='my-2 rounded-md px-4 py-2 border-2 border-marrom' />
-                    <input type="password" name="" id="" placeholder='Senha' className='my-2 rounded-md px-4 py-2 border-2 border-marrom' />
-                    <button className='bg-vinho px-4 py-2 rounded-md text-white'>ENTRAR</button>
+                    <button className='bg-vinho px-4 py-2 rounded-md text-white'>RECUPERAR</button>
                 </form>
-                <div className='my-10'>
-                    <a href="#" className='text-vinho'>Esqueceu sua senha?</a>
-                </div>
             </div>
         </div>
     );
